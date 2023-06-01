d2b2f4b736e8cd5023b19102866805b197bf4281
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-06-01 06:49:46 +0530

    config changes

--- BEProject/src/routes/v1/index.js
+++ BEProject/src/routes/v1/index.js
@@ -1,74 +1,12 @@
 const express = require("express");
-const videoRoute = require("./video.route");
-const userRoute = require("./user.route");
-const docsRoute = require("./docs.route");
-const categoryRoute = require("./category.route");
-const commonRoute = require("./common.route");
-const gameRoute = require("./game.route");
 const cmsRoute = require("./cms.route");
-const artistRoute = require("./artist.route");
-const playlistRoute = require("./playlist.route");
-const search = require("./search.route.js");
-const support = require("./support.route.js");
-const elastic = require("./elastic.route.js");
-
-
 const config = require("../../config/config");
 
 const router = express.Router();
 const defaultRoutes = [
 	{
-		path: "/video",
-		route: videoRoute
-	},
-	{
-		path: "/user",
-		route: userRoute
-	},
-	{
-		path: "/category",
-		route: categoryRoute
-	},
-	{
-		path: "/common",
-		route: commonRoute
-	},
-	{
-		path : '/game',
-		route : gameRoute	
-
-	},
-	{
 		path: "/cms",
 		route: cmsRoute
-	},
-	{
-		path: "/artist",
-		route: artistRoute
-	},
-	{
-		path: "/playlist",
-		route: playlistRoute
-	},
-	{
-		path: "/search",
-		route: search
-	},
-	{
-		path: "/support",
-		route: support
-	},
-	{
-		path: "/elastic",
-		route: elastic
-	}
-];
-
-const devRoutes = [
-	// routes available only in development mode
-	{
-		path: "/docs",
-		route: docsRoute,
 	},
 ];
 
